608358626693018dd685003afcd88b0fd73e0934
Đặng Quang Hưng <hungdq58@gmail.com>
2020-09-28 16:10:21 +0700

    Query Parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from typing import Optional
 from fastapi import FastAPI
 
 
@@ -17,9 +17,34 @@ async def root():
     return {"message": "Hello World"}
 
 
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# When you declare other function parameters that are not part of the path parameters, they are automatically interpreted as "query" parameters.
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip+limit]
+
+
+# Optional parameters
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+
+# Required query parameters
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
 
 
 # Order matters
@@ -31,6 +56,19 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/model/{model_name}")
